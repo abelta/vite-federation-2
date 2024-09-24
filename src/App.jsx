0a51cea9c6188ae32c868b4f491a5c6e9a4fafb1
Abel <abel.tamayo@gmail.com>
2024-09-24 15:41:39 +0200

    Implemented navigation.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>REMOTE 2</h1>
+      <Link to="/">App1</Link>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'remote2',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',
+      },
+      remotes: {
+        host: 'http://localhost:4999/assets/remoteEntry.js',
+      },
+      shared: [
+        'react',
+        'react-dom',
+        'react-router-dom',
+      ],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  },
+})
